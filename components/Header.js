909278239112ae0826eced77e23f428568b14b31
ActuallySam <srivastava.samarth.btech2018@sitpune.edu.in>
2021-08-13 00:24:34 +0530

    Calendar & Dates Added

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,37 @@
 import Image from "next/image";
-import { SearchIcon, MenuIcon, GlobeAltIcon, UserCircleIcon } from "@heroicons/react/solid";
+import { SearchIcon, MenuIcon, GlobeAltIcon, UsersIcon, UserCircleIcon } from "@heroicons/react/solid";
+import { useState } from "react";
+import { DateRangePicker } from "react-date-range";
+import 'react-date-range/dist/styles.css';  //main style file
+import 'react-date-range/dist/theme/default.css';   //theme css file
 
 function Header() {
+
+    //To define the search input and hold its value as *blank* when refreshed
+    const [searchInput, setSearchInput] = useState("");
+
+    //To define startDate and endDate as different dates for the date selection process
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    //For Selecting the range of the dates
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    };
+
+    //showing the startDate and endDate range on the calendar
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    };
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
 
             {/* left */}
-            <div className='relative flex ite h-10 cursor-pointer my-auto'>
+            <div className='relative flex items-center h-10 cursor-pointer my-auto md:'>
                 <Image 
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png" 
                     layout="fill"
@@ -16,8 +41,16 @@ function Header() {
 
 
             {/* middle */}
-            <div className="flex item-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input type="text" placeholder="Start your search" className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" />
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                <input 
+                    value={searchInput} 
+                    onChange={(event) => {
+                        setSearchInput(event.target.value)
+                    }}
+                    type="text" 
+                    placeholder="Start your search" 
+                    className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" />
+
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-300 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
 
@@ -33,6 +66,16 @@ function Header() {
                 </div>
 
             </div>
+
+            {searchInput && (
+                <div className="flex flex-col col-span-3 mx-auto mt-5">
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={["#fd5b61"]}
+                        onChange={handleSelect} />
+                </div>
+            )}
 
         </header>
     );
